25f479f23f777a0614c006c0d745063aee19e042
Chris Hultberg <71258025+chrishultberg@users.noreply.github.com>
2024-10-18 14:45:15 -0500

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$Settings = [
+    'author' => ['Chris Hultberg'],
+    'phone' => ['425-530-4500'],
+    'email' => ['chrishultberg@gmail.com'],
+    'website' => ['https://www.Ez-IT-Solutions.com']
+];
+?>
